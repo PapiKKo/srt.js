381eb0ee6249c12aee95a512320f11543ad810eb
PapiKKo <65007528+PapiKKo@users.noreply.github.com>
2021-10-14 15:26:33 +0900

    Add files via upload

---
+++ with_video/getQuery.js
@@ -0,0 +1,27 @@
+function execute() {
+    var yoga = document.sampleForm.menu[0];
+    var kintore = document.sampleForm.menu[1];
+    var bgm = document.sampleForm.menu[2];
+    var result = document.getElementById("result");
+    result.innerHTML = "";
+    var none = true;
+    const textbox = document.getElementById("input-message");
+    const inputValue = textbox.value;
+    const url = new URL(inputValue);
+    const params = new URLSearchParams(url.search);
+
+    if(yoga.checked) {
+        result.innerHTML = yoga.value + "が選択されています。";
+	window.open("http://localhost:4245/srt.js/index.htm?"+params+"&surl=localhost:4245/srt.js/sample/example2_srt.js",'_blank');
+        none = false;
+    }
+    if(kintore.checked) {
+        result.innerHTML += kintore.value + "が選択されています。";
+        none = false;
+    }
+    if(bgm.checked) {
+        result.innerHTML += bgm.value + "が選択されています。";
+	window.open("http://localhost:4245/srt.js/index.htm?"+params+"&surl=localhost:4245/srt.js/sample/example3_srt.js",'_blank');
+        none = false;
+    }
+}
